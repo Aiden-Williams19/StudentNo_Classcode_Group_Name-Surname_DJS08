ef699953634030f86d40c49c3fce5327397f9c8a
Aiden-Williams19 <aidenwilliams336@gmail.com>
2025-01-29 16:51:09 +0200

    Vans page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />} />
         </Routes>
       </BrowserRouter>
     )
