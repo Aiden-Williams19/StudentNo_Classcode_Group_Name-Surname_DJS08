04727bbf8b54f9b0ed73f364dd21829e036a319d
Aiden-Williams19 <aidenwilliams336@gmail.com>
2025-01-30 11:31:53 +0200

    added components for layout routes

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -10,24 +10,25 @@ export default function Vans() {
             .then(data => setVans(data.vans));
     }, []);
 
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`} className="van-link">
+                <img src={van.imageUrl} alt={van.name} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ));
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list">
-                {vans.map(van => (
-                    <div key={van.id} className="van-tile">
-                        <Link to={`/vans/${van.id}`} className="van-link">
-                            <img src={van.imageUrl} alt={van.name} />
-                            <div className="van-info">
-                                <h3>{van.name}</h3>
-                                <p>${van.price}<span>/day</span></p>
-                            </div>
-                            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                        </Link>
-                    </div>
-                ))}
+                {vanElements}
             </div>
         </div>
     );
 }
-
