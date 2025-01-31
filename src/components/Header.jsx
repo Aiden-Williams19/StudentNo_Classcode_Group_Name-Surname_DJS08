b59170edbda9f40ef300bee8cb0c9365de4d6937
Aiden-Williams19 <aidenwilliams336@gmail.com>
2025-01-31 13:59:33 +0200

    login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,41 @@
 import React from "react"
-import { Link, NavLink} from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
                 <NavLink 
                     to="/host"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
                     to="/about"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
                     to="/vans"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src="./assets/images/avatar-icon.png" 
+                        className="login-icon"
+                    />
+                </Link>
             </nav>
         </header>
     )
